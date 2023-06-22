8d4d643f5a994fe2ca833325b883434a3669bf46
Alfinaditya <alfinditia02@gmail.com>
2023-06-22 21:32:34 +0700

    styling something

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,11 @@
 @extends('app')
 @section('content')
     <div id="about" style="width:90%;margin:auto;margin-bottom:100px;padding-top:120px">
-        <h1 class="text-center" style="margin-bottom:10px">ABOUT ORRIN PARFUME</h1>
+        <h2 class="text-center" style="margin-bottom:25px;margin-top:25px">ABOUT ORRIN PARFUME</h2>
         <div style="display:flex;align-items: center;">
-            <p style="margin-right:100px">Orrin Parfume brand lokal Surabaya yang didirikan pada akhir tahun 2021. Misi utama
-                Orrin Parfume adalah menyediakan parfum berkualitas tinggi dengan menggunakan bahan-bahan terbaik dan harga
-                yang terjangkau.
+            <p style="margin-right:100px">Orrin Parfume brand parfum lokal asal Surabaya yang didirikan pada akhir tahun
+                2021. Misi utama Orrin Parfume adalah menyediakan parfum berkualitas tinggi dengan menggunakan bahan-bahan
+                terbaik dan harga yang terjangkau.
 
                 Kualitas merupakan pijakan utama yang dipegang teguh oleh Orrin Parfume. Dengan berbekal pengetahuan
                 mendalam tentang industri parfum dan pemahaman yang kuat tentang preferensi dan kebutuhan konsumen, Orrin
