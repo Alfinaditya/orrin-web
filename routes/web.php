b16bd92a14065218b25fd12ec1f060c3dde55469
jagoanemakk <jagoanemak1234@gmail.com>
2023-06-24 06:48:54 +0700

    remove unused code

--- routes/web.php
+++ routes/web.php
@@ -91,82 +91,12 @@ Route::post('/dashboard/product/{id}', [ProductController::class, 'update']);
 Route::get('/dashboard/product/{id}', [ProductController::class, 'destroy']);
 
 
-//Route Dashboard Mens Parfume
-
-Route::get('/dashboard/mens', [MensParfumeController::class, 'index']);
-
-Route::get('/dashboard/mens/create', [MensParfumeController::class, 'create']);
-
-Route::post('/dashboard/mens', [MensParfumeController::class, 'store']);
-
-Route::get('dashboard/mens/{id}/index', [MensParfumeController::class, 'show']);
-
-Route::get('dashboard/mens/{id}/edit', [MensParfumeController::class, 'edit']);
-
-Route::post('dashboard/mens/{id}', [MensParfumeController::class, 'update']);
-
-Route::get('dashboard/mens/{id}', [MensParfumeController::class, 'destroy']);
-
-//Route Dashboard Womens Parfume
-
-Route::get('/dashboard/womens', [WomensParfumeController::class, 'index']);
-
-Route::get('/dashboard/womens/create', [WomensParfumeController::class, 'create']);
-
-Route::post('/dashboard/womens', [WomensParfumeController::class, 'store']);
-
-Route::get('dashboard/womens/{id}/show', [WomensParfumeController::class, 'show']);
-
-Route::get('dashboard/womens/{id}/edit', [WomensParfumeController::class, 'edit']);
-
-Route::post('dashboard/womens/{id}', [WomensParfumeController::class, 'update']);
-
-Route::get('dashboard/womens/{id}', [WomensParfumeController::class, 'destroy']);
-
-//Route Dashboard Sweets Parfume
-
-Route::get('/dashboard/sweets', [SweetsParfumeController::class, 'index']);
-
-Route::get('/dashboard/sweets/create', [SweetsParfumeController::class, 'create']);
-
-Route::post('/dashboard/sweets', [SweetsParfumeController::class, 'store']);
-
-Route::get('dashboard/sweets/{id}/show', [SweetsParfumeController::class, 'show']);
-
-Route::get('dashboard/sweets/{id}/edit', [SweetsParfumeController::class, 'edit']);
-
-Route::post('dashboard/sweets/{id}', [SweetsParfumeController::class, 'update']);
-
-Route::get('dashboard/sweets/{id}', [SweetsParfumeController::class, 'destroy']);
-
-//Route Dashboard Casuals Parfume
-
-Route::get('/dashboard/casuals', [CasualsParfumeController::class, 'index']);
-
-Route::get('/dashboard/casuals/create', [CasualsParfumeController::class, 'create']);
-
-Route::post('/dashboard/casuals', [CasualsParfumeController::class, 'store']);
-
-Route::get('dashboard/casuals/{id}/show', [CasualsParfumeController::class, 'show']);
-
-Route::get('dashboard/casuals/{id}/edit', [CasualsParfumeController::class, 'edit']);
-
-Route::post('dashboard/casuals/{id}', [CasualsParfumeController::class, 'update']);
-
-Route::get('dashboard/casuals/{id}', [CasualsParfumeController::class, 'destroy']);
-
 Route::prefix('dt')->group(function () {
     Route::get('/kategori-parfume', [CategoriesController::class, 'listParfume']);
     Route::get('/jenis-parfume', [CategoryProductController::class, 'listParfume']);
     Route::get('/product', [ProductController::class, 'listParfume']);
-    Route::get('/womens-parfume', [WomensParfumeController::class, 'listParfume']);
-    Route::get('/sweets-parfume', [SweetsParfumeController::class, 'listParfume']);
-    Route::get('/casuals-parfume', [CasualsParfumeController::class, 'listParfume']);
 });
 
 Route::prefix('ajax')->group(function () {
     Route::get('/product-preview', [ProductController::class, 'ajaxParfume']);
-    Route::get('/womens-preview', [WomensParfumeController::class, 'ajaxParfume']);
-    Route::get('/sweets-preview', [SweetsParfumeController::class, 'ajaxParfume']);
-    Route::get('/casuals-preview', [CasualsParfumeController::class, 'ajaxParfume']);
 });
